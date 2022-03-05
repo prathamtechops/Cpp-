c78ef3e62ad6d0fc297081a3d2e3917b245834d3
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-03-05 16:55:49 +0530

    C

---
+++ Practice_Tutorial/if_else_elif.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int saving;
+    cout<<"Enter saving"<<endl;
+    cin>>saving;
+
+    if (saving > 5000){
+        cout<<"Neha"<<endl;
+    } else {
+        cout<<"Rashmi"<<endl;
+    }
+    return 0;
+}
