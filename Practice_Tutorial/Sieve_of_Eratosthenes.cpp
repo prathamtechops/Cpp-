25584fd65453e86027bece347c235074380672de
Pratham-Techops <88659230+Pratham-Techops@users.noreply.github.com>
2022-04-15 18:50:12 +0530

    C

--- Practice_Tutorial/Sieve_of_Eratosthenes.cpp
+++ Practice_Tutorial/Sieve_of_Eratosthenes.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void prime(int n);
+int prime(int *n)
 {
-    int prime[100] = {0};
+    for (int i = 3; i <= *n; i = i + 2){
+        p[i] = 1;
+        if(p[i] == 1){
+            for(int j = i*i; j <= *n; j = j + i){
+                p[j] = 0;
+            }
+        }
+    }
+    
 }
 
 int main(int argc, char const *argv[])
 {
-    
+    int n;
+    cin >> n;
+    int p[n] = {0};
+    prime(p);
+    for (int i = 0; i <= n; i++){
+        if (p[i] == 1 ) cout<< i<<" ";
+    }
     return 0;
 }
